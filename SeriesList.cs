f933e62c18d0aae9bee9679d547c17c2ed0cea6e
DJDoena <github@doena-soft.de>
2023-08-03 19:23:44 +0200

    MediaInfoHelper 3.0.0

--- SeriesList.cs
+++ SeriesList.cs
@@ -1,14 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using DoenaSoft.MediaInfoHelper.DataObjects.VideoMetaXml;
+using DoenaSoft.MediaInfoHelper.Helpers;
+
 namespace DoenaSoft.SeriesList
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Xml.Serialization;
-    using MediaInfoHelper;
-
     public static class SeriesList
     {
         private static readonly XmlSerializer _serializer;
